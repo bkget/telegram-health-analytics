f098e146ecb947a0f6108d7391850c3d93407e93
Biruk Getaneh <bkgetmom@gmail.com>
2025-07-24 21:10:40 +0300

    latest changes

--- telegram_dbt/models/staging/stg_telegram_messages.sql
+++ telegram_dbt/models/staging/stg_telegram_messages.sql
@@ -1,16 +1,20 @@
+{{ config (materialized = "table") }}
+
 with raw as (
 
   select
     id,
-    -- Remove the '.json' extension from source_file
     regexp_replace(source_file, '\.json$', '') as source_file,
-    -- Extract fields from JSON message
     (message->>'date')::timestamp as date,
     message->>'text' as text,
     (message->>'views')::int as views,
-    (message->>'from_id')::int as from_id,
+    
+    -- extracts only the digits from the from_id field before casting
+    regexp_replace(message->>'from_id', '\D', '', 'g')::bigint as from_id,
+
     (message->>'replies')::int as replies,
     (message->>'forwards')::int as forwards
+
   from public.raw_telegram_messages
 
 )
